80fca20713b8e43edcef1ea024d1df1802b81836
JordiMartinezDev <jordimartinezdev@gmail.com>
2023-11-23 06:36:21 +0100

    ErrorHandling

--- contracts/TryCatch.sol
+++ contracts/TryCatch.sol
@@ -4,6 +4,8 @@ pragma solidity 0.8.22;
 
 contract TryCatch{
 
+    error ErrorBytes(string);
+
     mapping(address => uint) public balanceReceived;
 
     function receiveMoney() public payable{
@@ -14,5 +16,35 @@ contract TryCatch{
         require(_amount <= balanceReceived[msg.sender], "Not enough founds, reverting!");
         balanceReceived[msg.sender] -= _amount;
         _to.transfer(_amount);
+    }
+
+    function throwErrorFunction() public pure{
+        //require(false, " error");
+        //assert(false);
+        revert ErrorBytes("Not Catchable error, but reverting everything here");
+    }
+
+
+}
+
+contract ErrorHandling{
+    event ErrorRequire(string reason);
+    event ErrorAssertCode(uint code);
+    event ErrorBytes(bytes code);
+    function catchTheError() public{
+        TryCatch throwError = new TryCatch();
+        
+        try throwError.throwErrorFunction(){
+
+        }catch Error(string memory reason){
+
+            emit ErrorRequire(reason);
+        }catch Panic(uint errorCode){
+            emit ErrorAssertCode(errorCode);
+        }catch(bytes memory lowLevelData){
+            emit ErrorBytes(lowLevelData);
+        }
+
+        
     }
 }
