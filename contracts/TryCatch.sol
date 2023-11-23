8476e119ad234c0457794727db773e2179218634
JordiMartinezDev <jordimartinezdev@gmail.com>
2023-11-23 06:08:34 +0100

    ReceiveTransferRequired

---
+++ contracts/TryCatch.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.22;
+
+contract TryCatch{
+
+    mapping(address => uint) public balanceReceived;
+
+    function receiveMoney() public payable{
+        assert(msg.value == uint8(msg.value));
+        balanceReceived[msg.sender] += uint8(msg.value);
+    }
+    function withdrawMoney(address payable _to, uint8 _amount) public{
+        require(_amount <= balanceReceived[msg.sender], "Not enough founds, reverting!");
+        balanceReceived[msg.sender] -= _amount;
+        _to.transfer(_amount);
+    }
+}
